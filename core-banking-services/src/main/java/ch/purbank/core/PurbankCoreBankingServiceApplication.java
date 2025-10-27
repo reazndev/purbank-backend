43a0ee6f95b49b2a8a8a8f8fd87cdd325dfe69b2
David Koteski <142885534+seakyy@users.noreply.github.com>
2025-10-27 14:29:11 +0100

    Projekt aufsetzen
    Ordner inkl. Dependencies (siehe cloud.hilfiker.com -> Backend -> Spring.md)
    Bereit zum starten

---
+++ core-banking-services/src/main/java/ch/purbank/core/PurbankCoreBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.purbank.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurbankCoreBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurbankCoreBankingServiceApplication.class, args);
+	}
+
+}
