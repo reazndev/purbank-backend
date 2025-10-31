a5580db5eefd7150967395b156252aeb9ea9181e
Lukas Hilfiker <lukas@hilfiker.dev>
2025-10-31 15:48:23 +0100

    openapi setup

---
+++ core-banking-services/src/main/java/ch/purbank/core/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package ch.purbank.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI purbankOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Purbank Core Banking API")
+                        .description("REST API for Purbank core banking services")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Purbank Dev Team")
+                                .email("dev@purbank.ch")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Development Server")));
+    }
+}
