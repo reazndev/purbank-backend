faef9a999990a4d100de5400e4fca58b4fa050e4
Lukas Hilfiker <lukas@hilfiker.dev>
2025-11-03 13:26:35 +0100

    MobileDevice domain

---
+++ core-banking-services/src/main/java/ch/purbank/core/domain/MobileDevice.java
@@ -0,0 +1,40 @@
+package ch.purbank.core.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mobile_device")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MobileDevice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private java.util.UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String publicKey;
+
+    @Column(name = "device-name", nullable = false)
+    private String deviceName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "connected_user", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String status = "ACTIVE";
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
