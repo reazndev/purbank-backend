9abceeaf7867219e9b2697a70b8fd75c42dc7b1b
David Koteski <142885534+seakyy@users.noreply.github.com>
2025-12-01 13:43:51 +0100

    Add validation annotations to AuthenticationRequestDTO

--- core-banking-services/src/main/java/ch/purbank/core/dto/AuthenticationRequestDTO.java
+++ core-banking-services/src/main/java/ch/purbank/core/dto/AuthenticationRequestDTO.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
